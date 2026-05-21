55e28b4c11b3b0297a6b68e35aa556dbe05f12a8
marsil1990 <ma.silveram@gmail.com>
2026-05-21 20:07:40 -0300

    images

--- src/components/BenefitsSection.tsx
+++ src/components/BenefitsSection.tsx
@@ -1,19 +1,24 @@
+import Image from "next/image";
 const benefits = [
   {
     title: "Support Artisans",
     text: "Help local makers thrive",
+    img: "support.webp"
   },
   {
     title: "Unique & Authentic",
     text: "One-of-a-kind creations",
+    img: "unique.webp"
   },
   {
     title: "Quality You Can Trust",
     text: "Crafted with care",
+    img: "quality.webp"
   },
   {
     title: "Worldwide Community",
     text: "Connect globally",
+    img: "world.webp"
   },
 ];
 
@@ -23,7 +28,14 @@ export default function BenefitsSection() {
         <section className="benefits">
             {benefits.map((benefit) => (
                 <article key={benefit.title}>
-                  <div className="benefit-icon"></div>
+                <div className="benefit-icon">
+                  <Image
+                    src={`/${benefit.img}`}
+                    width={52}
+                    height={52}
+                    alt="Handcrafted ceramic vase and wooden bowls on a rustic table"
+                    loading="eager" />
+                  </div>
                   <h3>{benefit.title}</h3>
                   <p>{benefit.text}</p>
                 </article>
