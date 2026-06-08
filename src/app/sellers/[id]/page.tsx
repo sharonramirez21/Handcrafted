58229e59842c665814a0d2c828ad96c6f6255b66
marsil1990 <ma.silveram@gmail.com>
2026-06-08 13:50:54 -0300

    update skeleton

--- src/app/sellers/[id]/page.tsx
+++ src/app/sellers/[id]/page.tsx
@@ -30,6 +30,7 @@ export default async function Page({
               fill
               className="seller-image"
               loading="eager"
+              sizes="(max-width: 768px) 100vw, 300px"
             />
           </div>
 
